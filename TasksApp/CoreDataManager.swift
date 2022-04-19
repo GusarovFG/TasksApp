1a2f0d4339da6564cc0b45c4b0d82a92e84ed896
Фаддей Гусаров <gusarovfg@gmail.com>
2022-04-19 15:42:08 +0400

    feat: Реализован CoreDataManager для работы с базой данных приложения

---
+++ TasksApp/CoreDataManager.swift
@@ -0,0 +1,91 @@
+//
+//  CoreDataManager.swift
+//  TasksApp
+//
+//  Created by Фаддей Гусаров on 19.04.2022.
+//
+import CoreData
+import Foundation
+import UIKit
+
+
+class CoreDataManager {
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "TasksApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    //MARK: - Core Data fetching data
+    
+    func fetchTasks() -> [Task] {
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let tasks = (try? self.persistentContainer.viewContext.fetch(fetchRequest)) ?? []
+        
+        return tasks
+    }
+    
+    //MARK: - Core Data saving data
+    
+    func saveTask(title: String, text: String, image: UIImage) {
+        
+        let task = Task(context: self.persistentContainer.viewContext)
+        let imageData = UIImage.pngData(image)
+        
+        task.title = title
+        task.text = text
+        task.images = imageData()
+        task.creationDate = DateManager.shared.getDate()
+        
+        saveContext()
+    }
+    
+    //MARK: - Core Data delete data
+
+    func deleteTask(index: Int) {
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let tasks = (try? self.persistentContainer.viewContext.fetch(fetchRequest)) ?? []
+        
+        self.persistentContainer.viewContext.delete(tasks[index])
+        
+        saveContext()
+    }
+    
+    //MARK: - Core Data edit data
+    
+    func editTask(index: Int, task: Task) {
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        var tasks = (try? self.persistentContainer.viewContext.fetch(fetchRequest)) ?? []
+        
+        tasks[index] = task
+        
+        saveContext()
+    }
+}
+
+
